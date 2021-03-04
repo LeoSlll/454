98e081cc38a45df268d0d96c2100a2288fb9c834
LeoSlll <74057924+LeoSlll@users.noreply.github.com>
2021-03-04 18:48:55 +0300

    343
    prjct

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+album.onmouseover = function(){
+   let target = this.id;
+   if (target == 'album')
+   body.background = '../img/7SIm.gif';
+}
+
+
+album2.onmouseover = function(){
+   let target = this.id
+   if (target == 'album2')
+   body.background = '../img/IMG_4410.gif';
+;
+
+}
+
+
+
+   
+  album.onmouseout = function(){
+  let target = body;
+  target.style.background = '';
+   }
+
+
+
+    
+
