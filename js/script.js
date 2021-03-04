e6d5315a1dcdd581f74f55b95daa984062657a36
LeoSlll <74057924+LeoSlll@users.noreply.github.com>
2021-03-04 20:11:39 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 album.onmouseover = function(){
    let target = this.id;
    if (target == 'album')
-   body.background = '../img/fon.GIF';
+   body.background = './img/fon.gif';
                      
 }
 
@@ -10,7 +10,7 @@ album.onmouseover = function(){
 album2.onmouseover = function(){
    let target = this.id
    if (target == 'album2')
-   body.background = '../img/foon.GIF';
+   body.background = './img/foon.gif';
 ;
 
 }
