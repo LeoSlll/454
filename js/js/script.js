0cd4d428691a5e10debdf3441ea7e0290628f97c
f.Shigarcov <74057924+LeoSlll@users.noreply.github.com>
2021-03-16 18:44:19 +0300

    Add files via upload

---
+++ js/js/script.js
@@ -0,0 +1,30 @@
+
+
+
+body.background = './img/1.gif';
+document.body.style.background
+
+
+
+ 
+
+album2.onmouseover = function(){
+   let target = this.id
+   if (target == 'album2')
+   body.background = './img/2.gif';
+;
+
+}
+
+
+
+   
+  album2.onmouseout = function(){
+  let target = body;
+  target.background = './img/1.gif';
+   }
+
+
+
+    
+
